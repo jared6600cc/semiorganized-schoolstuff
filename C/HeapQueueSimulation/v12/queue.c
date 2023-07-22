59347c134c199464e459a52bba89c0b8fcadd695
jared6600cc <135299193+jared6600cc@users.noreply.github.com>
2023-07-22 01:36:25 -0500

    Add files via upload

---
+++ C/HeapQueueSimulation/v12/queue.c
@@ -0,0 +1,18 @@
+#include "queue.h"
+#include <stdlib.h>
+
+struct node *front = NULL;
+struct node *rear;
+
+
+int enqueue(struct node * add){
+	return 0;
+}
+
+int dequeue(){
+	return 0;
+}
+
+int isEmpty(){
+	return 0;
+}
