2d39c7ed1c0795a982472a4869e22bb7caa1f72c
jared6600cc <135299193+jared6600cc@users.noreply.github.com>
2023-07-22 00:59:45 -0500

    Add files via upload

---
+++ C/HeapQueueSimulation/v6/simulation.h
@@ -0,0 +1,19 @@
+#ifndef simulation
+#define simulation
+
+struct Customer{
+	float arrivalTime;
+	float startOfServiceTime;
+	float departureTime;
+	struct Customer * next;
+};
+
+struct Event{
+	int type;
+	struct Customer * cust;
+};
+
+int processNextEvent();
+float getNextRandomInterval();
+
+#endif
