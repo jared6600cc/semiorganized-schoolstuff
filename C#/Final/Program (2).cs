5f225a7e02a26481a895ceadc6202df79728369f
jared6600cc <135299193+jared6600cc@users.noreply.github.com>
2023-07-22 23:24:54 -0500

    Add files via upload

---
+++ C#/Final/Program (2).cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get a list of:
+            //---all the titles and the authors who wrote them (sort by title)
+            //---all the titles and the authors who wrote them (sort by title)(for each title sort the authors alphabetically by last name, the first name
+            //---all the authors grouped by title, sorted by title; for a given title sort the author names alphabetically by last name then first name
+        }
+    }
+}
---
+++ C#/Final/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayTable
+{
+    public partial class DisplayAuthorsTable : Form
+    {
+        //constructor
+        public DisplayAuthorsTable()
+        {   
+        }
+
+        //Entity Framework DbContext
+        private FinalProj.BooksEntities dbcontext = new FinalProj.BooksEntities();
+
+        //load data from database into DataGridView
+        private void DisplayTable_Load( object sender, EventArgs e)
+        {
+            //load authors table ordered by LastName then FirstName
+            dbcontext.Authors
+                .OrderBy(author => author.LastName)
+                .ThenBy(author => author.FirstName)
+                .Load();
+
+            //specify DataSource for authorBindingSource
+            authorBindingSource.DataSource = dbcontext.Authors.Local;
+        }
+
+        //click event handler for the Save Button in the
+        //BindingNavigator saves the changes made to the data
+        private void authorBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            //validate input fields
+            Validate(); //validate the input fields
+            authorBindingSource.EndEdit(); ////complete current edit, if any
+            // try to save changes
+            try
+            {
+                dbcontext.SaveChanges();
+            }
+            catch (DbEntityValidationException)
+            {
+                MessageBox.Show("FirstName and LastName must contain values", "Entity Validation Exception");
+            }
+        }
+    }
+}
