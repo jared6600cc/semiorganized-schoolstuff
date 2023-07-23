44a2ae12ab193e068c0b599a448d289b843b636e
jared6600cc <135299193+jared6600cc@users.noreply.github.com>
2023-07-22 22:46:06 -0500

    Add files via upload

---
+++ C++/MyShell/v32/commands.hpp
@@ -0,0 +1,25 @@
+//
+//  commands.hpp
+//  Project1
+//
+//  Created by Jared Bumgardner on 9/19/19.
+//  Copyright © 2019 Jared Bumgardner. All rights reserved.
+//
+
+#ifndef commands_hpp
+#define commands_hpp
+
+#include <stdio.h>
+#include "directory.hpp"
+
+void ls(Node lsThis);
+void mkdir();
+void cd();
+void pwd();
+void addf();
+void mv();
+void cp();
+void rm();
+void whereis();
+
+#endif /* commands_hpp */
