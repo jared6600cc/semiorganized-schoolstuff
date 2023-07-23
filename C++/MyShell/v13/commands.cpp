f3f45e05cb7411c5ac09a88a1441b4b0fed53f32
jared6600cc <135299193+jared6600cc@users.noreply.github.com>
2023-07-22 22:28:05 -0500

    Add files via upload

---
+++ C++/MyShell/v13/commands.cpp
@@ -0,0 +1,20 @@
+//
+//  commands.cpp
+//  Project1
+//
+//  Created by Jared Bumgardner on 9/19/19.
+//  Copyright © 2019 Jared Bumgardner. All rights reserved.
+//
+
+#include "commands.hpp"
+#include <iostream>
+
+void ls() {std :: cout << "**ls**\n";}
+void mkdir() {std :: cout << "**mkdir**\n";}
+void cd() {std :: cout << "**cd**\n";}
+void pwd() {std :: cout << "**pwd**\n";}
+void addf() {std :: cout << "**addf**\n";}
+void mv() {std :: cout << "**mv**\n";}
+void cp() {std :: cout << "**cp**\n";}
+void rm() {std :: cout << "**rm**\n";}
+void whereis() {std :: cout << "**whereis**\n";}
